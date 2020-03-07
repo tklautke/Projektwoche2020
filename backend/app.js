76d4cb9c25f686767fda4f3c536f41d3da40b542
Theo Klautke <theoklautkemail@gmail.com>
2020-03-07 17:51:22 +0100

    added some frontend and backend stuff

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,31 @@
-//Backend entry point
+const express = require('express');
+const bodyParser = require('body-parser');
+const  app = express();
+
+/*This is the port where the page starts */
+const  port = 8080;
+app.listen(port, ()=> console.log(`Server started at port: ${port}`));
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    next();
+});
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//app.use(express.static(__dirname + '../frontend/'));
+app.use(express.static( '/Users/theoklautke/Projekte/Projektwoche2020/source/frontend/'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/Users/theoklautke/Projekte/Projektwoche2020/source/frontend/index.html')
+});
+
+
 
 
--- frontend/registration/registration.js
+++ frontend/registration/registration.js
@@ -1,12 +1,27 @@
+/**
+ *  @link main
+ *  @author tklautke
+ *  @description this function detects if the dom is fully loaded.
+ */
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     main()
 });
 
+/**
+ *  @link valueFromInput
+ *  @author tklautke
+ *  @description this is the main function, which is includes the "btn" blick event
+ */
 function main() {
     document.getElementById("btn").addEventListener("click", valueFromInput);
 }
 
+/**
+ *  @link convertValueFromInputIntoJson
+ *  @author tklautke
+ *  @description this function is getting the values of the registration input fields
+ */
 function valueFromInput() {
     const firstName = document  = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
@@ -16,12 +31,20 @@ function valueFromInput() {
     console.log(`The Value for Firstname is: ${firstName}`);
     console.log(`The Value for Lastname  is: ${lastName}`);
     console.log(`The Value for Username is: ${userName}`);
-    console.log(`The Value for Password is: ${password}`)
+    console.log(`The Value for Password is: ${password}`);
 
-    convertValueFromInputintoJson(firstName, lastName, userName, password)
+    convertValueFromInputIntoJson(firstName, lastName, userName, password);
 }
 
-function convertValueFromInputintoJson(firstName, lastName, userName, password) {
+/**
+ * @param firstName
+ * @param lastName
+ * @param userName
+ * @param password
+ * @description is putting all input values of the registration into a JSON object.
+ * @author tklautke
+ */
+function convertValueFromInputIntoJson(firstName, lastName, userName, password) {
     const obj = {
         "FirstName": firstName,
         "LastName":lastName,
@@ -30,5 +53,3 @@ function convertValueFromInputintoJson(firstName, lastName, userName, password) 
     };
     console.log(obj)
 }
-
-
